9a790588aa3a880074fb6ba9ae167b1cf9a47175
MasterFan <343887511@qq.com>
2016-02-29 21:03:17 +0800

    add collection cell

---
+++ ICloudBook_ios/ReadCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ReadCollectionViewCell.h
+//  ICloudBook_ios
+//
+//  Created by 范国强 on 16/2/29.
+//  Copyright © 2016年 MasterFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReadCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *collectionViewCellImg;
+@property (weak, nonatomic) IBOutlet UILabel *collectionViewCellLabel;
+
+@end
---
+++ ICloudBook_ios/ReadCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  ReadCollectionViewCell.m
+//  ICloudBook_ios
+//
+//  Created by 范国强 on 16/2/29.
+//  Copyright © 2016年 MasterFan. All rights reserved.
+//
+
+#import "ReadCollectionViewCell.h"
+
+@implementation ReadCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end

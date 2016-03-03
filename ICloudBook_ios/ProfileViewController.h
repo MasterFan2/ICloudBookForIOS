7f5f36f5c8ec0b220302251ef285debd47a2902e
masterfan <343887511@qq.com>
2016-03-03 23:51:03 +0800

    add Profile tableview styles

--- ICloudBook_ios/ProfileViewController.h
+++ ICloudBook_ios/ProfileViewController.h
@@ -13,5 +13,6 @@
 }
 
 @property (nonatomic, retain) NSArray * dataList;
+@property (nonatomic, retain) NSDictionary * pDict;
 
 @end
--- ICloudBook_ios/ProfileViewController.m
+++ ICloudBook_ios/ProfileViewController.m
@@ -26,10 +26,29 @@
 //    self.navigationItem.title=@"个人信息设置";
 
 
-    ///读取plist中文件的内容。
+    ///load content from plist file.
     NSString * plistPath = [[NSBundle mainBundle] pathForResource:@"Profile" ofType:@"plist"];
-    NSMutableDictionary * dict = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
-    dataList = dict.allKeys;
+    self.pDict = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+    dataList = self.pDict.allKeys;
+    
+    
+    ///comparator
+//    NSComparator sort = ^(NSString *obj1,NSString *obj2){
+//        unichar obj1Char = [obj1 characterAtIndex:0];
+//        unichar obj2Char = [obj2 characterAtIndex:0];
+//        
+//        NSLog(@"obj1char=%c,     obj2Char=%c", obj1Char, obj2Char);
+//        
+//        if (obj1Char > obj2Char) {
+//            return NSOrderedDescending;
+//        }else if (obj1Char < obj2Char){
+//            return NSOrderedAscending;
+//        }else {
+//            return NSOrderedSame;
+//        }
+//    };
+//    
+//    [dataList sortedArrayUsingComparator:sort];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -56,11 +75,40 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString* identifier = @"profileIdentifier";
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    NSDictionary* tempDict = [self.pDict valueForKey:[dataList objectAtIndex:[indexPath row]]];
+    
+    NSNumber *type = [tempDict objectForKey:@"type"];
+    
+    NSLog(@"%@", type);
+    
     if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        if ([type compare:[NSNumber numberWithInt:0]] == NSOrderedSame) {       ///0.normal
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+            
+        } else if ([type compare:[NSNumber numberWithInt:1]] == NSOrderedSame) {///1.arrow
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.detailTextLabel.text = @"Ha.";
+            
+        } else if ([type compare:[NSNumber numberWithInt:2]] == NSOrderedSame) {///2.head picture
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+            cell.detailTextLabel.text = @"Ha.";
+            
+        } else if ([type compare:[NSNumber numberWithInt:3]] == NSOrderedSame) {///3.single label
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.detailTextLabel.text = @"";
+            
+        } else {///4                                                            ///4.double labels.
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifier];
+            cell.detailTextLabel.text = @"Ha.";
+        }
+        
     }
     
     cell.textLabel.text = [dataList objectAtIndex:indexPath.row];
+    
     return cell;
 }
 
